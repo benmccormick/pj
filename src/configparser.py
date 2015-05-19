0d63f3746468c54535af984e1df9db66af46b713
Ben McCormick <ben.mccormick@windsorcircle.com>
2015-05-18 23:18:37 -0400

    Add richer handling of .pjconfig files.  Closes #3 and #4

--- src/configparser.py
+++ src/configparser.py
@@ -2,15 +2,77 @@
 # For now it just processes a single included folder, without recursing it
 
 import os
+import os.path
 
-base_dir = os.getcwd()
 
-def get_file_list(config_data):
+def get_file_list(config_data, base_dir, ignored_files):
 
     project = config_data['files']
+    file_list = []
+    # we add included folders first, excluding ignored files
+    if 'included_folders' in project:
+        file_list.extend(include_folders(project['included_folders'], base_dir, ignored_files))
+    # then add included files, bypassing the ignored files list since they were
+    # added explicitly
+    if 'included_files' in project:
+        file_list.extend(include_files(project['included_files'], base_dir))
+    if 'excluded_folders' in project:
+        file_list = exclude_folders(file_list, project['excluded_folders'], base_dir)
+    if 'excluded_files' in project:
+        file_list = exclude_files(file_list, project['excluded_files'], base_dir)
+    
+    return deduplicate(file_list)
 
-    file_list = os.listdir(project['included_folders'][0])
 
-    file_list = map(lambda f: base_dir + '/' + f, file_list)
+def include_folders(folders, base_dir, ignored_files):
+    returned_files = []
+    for folder in folders:
+        full_path = normalize_path(folder, base_dir)
+        file_walk = os.walk(full_path) 
+        for directory in file_walk:
+            path = directory[0]
+            file_list = directory[2]
+            for file_name in file_list:
+                file_path = path + '/' + file_name
+                if not should_be_ignored(file_path, ignored_files):
+                    returned_files.append(file_path)
 
-    return file_list
+    return returned_files
+
+def include_files(files, base_dir):
+    included_files = map(lambda f: normalize_path(f, base_dir), files)
+    return filter(lambda f: not os.path.isfile(f), included_files)
+
+
+def exclude_folders(paths, excluded_folders, base_dir):
+    # this could be optimized later if the  paths were sorted in some way
+    excluded_folders = map(lambda f: normalize_path(f, base_dir), excluded_folders)
+    for exclude_folder in excluded_folders:
+        paths = filter(lambda p: not p.startswith(exclude_folder), paths)
+    return paths
+
+def exclude_files(paths, excluded_files, base_dir):
+    # this could be optimized later if the  paths were sorted in some way
+    excluded_files = map(lambda f: normalize_path(f, base_dir), excluded_files)
+    for exclude_file in excluded_files:
+        paths = filter(lambda p: not p == exclude_file, paths)
+    return paths
+
+
+
+def should_be_ignored(file_path, ignored_files):
+    for ignore_str in ignored_files:
+        if ignore_str in file_path:
+            return True
+    return False
+
+def normalize_path(path, base_dir):
+    full_path = ''
+    if path and path[0] == '/':
+       full_path = path 
+    else:
+       full_path = base_dir + '/' + path
+    return full_path
+
+def deduplicate(files):
+    return list(set(files))
--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 import projectconfig
 import configparser
 import listprint
+import os
 
+ignored_files = ['.git', 'node_modules', '.DS_Store']
 
 config_data = projectconfig.get_config()
 
@@ -9,7 +11,8 @@ if not config_data:
     print 'No .pjconfig file found'
     exit()
 
-file_list = configparser.get_file_list(config_data)
+base_dir = os.getcwd()
+file_list = configparser.get_file_list(config_data, base_dir, ignored_files)
 
 listprint.print_list(file_list)
 
