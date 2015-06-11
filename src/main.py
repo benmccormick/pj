e05282576a2e979791c1021678b88ce0a840e8fd
Ben McCormick <ben.mccormick@windsorcircle.com>
2015-06-10 22:22:56 -0400

    Rename files to differentiate and more accurately represent purpose

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-import projectconfig
-import configparser
-import listprint
+import configreader
+import projectbuilder
+import printer
 import os
 import sys
 
@@ -11,15 +11,15 @@ ignored_folders = ['.git', 'node_modules']
 filter = sys.argv[1] if len(sys.argv) >= 2 else None
 
 try:
-    [config_data, base_dir] = projectconfig.get_config()
+    [config_data, base_dir] = configreader.get_config()
 
     if not config_data:
         print('No .pjconfig file found')
         exit()
 
-    file_list = configparser.get_file_list(config_data, base_dir, filter, ignored_files, ignored_folders)
+    file_list = projectbuilder.get_file_list(config_data, base_dir, filter, ignored_files, ignored_folders)
 
-    listprint.print_list(file_list)
+    printer.print_list(file_list)
 except: 
     print('Exiting...')
 
